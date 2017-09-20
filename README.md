1e263df0812b5895abf8771e1aa461f8319ef4d6
Corey Martin <coreymartin496@gmail.com>
2017-09-19 21:12:08 -0400

    Updated code to us eproper Empoyee table

--- README.md
+++ README.md
@@ -1,39 +1,52 @@
 # JustCoreMarc
 Project to facilitate the bidding of bus runs
 
-## How to run
+## Run for development
 1. Setup .env file
 2. npm install
-3. npm run start
+3. npm run dev
 
 ## Services:
 
 ### /
 
-Method: GET
+GET / HTTP/1.1
+Host: localhost:3000
 
 ### /v1/createuser
 
-Method: POST 
-payload structure: 
+POST /v1/createuser HTTP/1.1
+Host: localhost:3000
+Content-Type: application/json
+
+payload structure:
 ```javascript
 {
-	"username" : "Foo",
-	"password" : "foo"
+	"firstName" : "Corey",
+	"lastName" : "Martin",
+	"email" : "Corey@god.com",
+	"dateOfBirth" : "1990/01/01",
+	"employeeNumber" : 2,
+	"username" : "Corey2",
+	"password" : "password"
 }
 ```
 
 ### /v1/login
 
-Method: POST
+POST /v1/login HTTP/1.1
+Host: localhost:3000
+Content-Type: application/json
+
 payload structure: 
 ```javascript
 {
-	"username" : "Foo",
-	"password" : "foo"
+	"username" : "Corey2",
+	"password" : "martinmartin"
 }
 ```
 
 ### /v1/logout
 
-Method: GET 
+GET /v1/logout HTTP/1.1
+Host: localhost:3000
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,8 +1,9 @@
 const session = require('express-session');
 const hash = require('pbkdf2-password')();
+const mysql = require('mysql');
 
 const con = require('../database/mysql').con;
-const employeeTable = "TempEmployees";
+const employeeTable = "EMPLOYEES";
 
 //Login
 exports.login = (req, res) => {
@@ -12,7 +13,7 @@ exports.login = (req, res) => {
     return res.send({message: `You are required to logout before you login ${req.session.username}`});
   }
 
-  //check if username and password was supplied
+  //check if username and password were supplied
   if (!req.body.username) {
     res.status(400);
     let error = {message: "username is required"};
@@ -23,8 +24,9 @@ exports.login = (req, res) => {
     return res.send(error);
   }
 
-  //TODO: sanitize input to prevent sql injections
-  const sql = `SELECT * FROM ${employeeTable} WHERE username = '${req.body.username}'`;
+  //using prepared statements to prevent sql injection
+  const params = [employeeTable, req.body.username];
+  const sql = mysql.format('SELECT * FROM ?? WHERE User_Name = ?', params);
   con.query(sql, function (err, result, fields) {
 
     if (err) {
@@ -41,12 +43,24 @@ exports.login = (req, res) => {
     }
 
     let user = result[0];
-    hash({password: req.body.password, salt: user.salt}, (err, pass, salt, hash) => {
+    hash({password: req.body.password, salt: user.Salt}, (err, pass, salt, hash) => {
       //check if hash matches
-      if (hash == user.hash) {
+      if (hash == user.Password) {
         req.session.regenerate(() => {
-          req.session.username = user.username; //store username in session
-          return res.send({message: `Welcome: ${req.body.username}`});
+          req.session.username = user.User_Name; //store username in session
+
+          //translate database fields to camal case values
+          let display = {};
+          display.employeeId = user.Employees_Id;
+          display.firstName = user.First_Name;
+          display.lastName = user.Last_Name;
+          display.email = user.Email;
+          display.dateOfBirth = user.Date_Of_Birth;
+          display.employeeNumber = user.Employee_Number;
+          display.seniority = user.Seniority;
+          display.username = user.User_Name;
+
+          return res.send(display);
         });
       } else {
         //wrong password
@@ -65,19 +79,46 @@ exports.creatuser = (req, res) => {
     return res.send({message: `You are required to logout before you register ${req.session.username}`});
   }
 
-  //check if username and password was supplied
-  if (!req.body.username) {
-    res.status(400);
-    let error = {message: "username is required"};
-    return res.send(error);
-  } else if (!req.body.password) {
-    res.status(400);
-    let error = {message: "password is required"};
-    return res.send(error);
+  let user = req.body;
+
+  //check if all required fields were entered
+  let errors = [];
+  if (!user.firstName) {
+    errors.push({error: 'firstName is required'});
+  }
+  if (!user.lastName) {
+    errors.push({error: 'lastName is required'});
+  }
+  if (!user.email) {
+    errors.push({error: 'email is required'});
+  }
+  if (!user.dateOfBirth) {
+    errors.push({error: 'dateOfBirth is required'});
+  }
+  if (!user.employeeNumber) {
+    errors.push({error: 'employeeNumber is required'});
+  } 
+  /*if (!user.seniority) {
+    errors.push({error: 'seniority is required'});
+  } */
+  if (!user.username) {
+    errors.push({error: 'username is required'});
+  }
+  if (!user.password) {
+    errors.push({error: 'password is required'});
   }
 
+  //check if any errors occured
+  if (errors.length) {
+    res.status(400)
+    return res.send(errors);
+  }
+
+  //using prepared statements to prevent sql injection
+  const params = [employeeTable, user.username];
+  const findUserSQL = mysql.format('SELECT User_Name FROM ?? WHERE User_Name = ?', params);
+
   //check if username is already taken
-  const findUserSQL = `SELECT username FROM ${employeeTable} WHERE username = '${req.body.username}'`;
   con.query(findUserSQL, function (err, result) {
     //Check for SQL errors
     if (err) { 
@@ -101,9 +142,12 @@ exports.creatuser = (req, res) => {
         return res.send(error);
       }
 
-      //TODO: sanitize input to prevent sql injections
-
-      const insertUserSQL = `INSERT INTO ${employeeTable} (username, hash, salt) VALUES ('${req.body.username}', '${hash}', '${salt}')`;
+      //using prepared statements to prevent sql injection
+      const params = [employeeTable, user.firstName, user.lastName, user.email, user.dateOfBirth, 
+                      user.employeeNumber, user.username, hash, salt];
+      const insertUserSQL = mysql.format('INSERT INTO ?? ' +
+      '(First_Name, Last_Name, Email, Date_Of_Birth, Employee_Number, User_Name, Password, Salt)' + 
+      'VALUES (?, ?, ?, ?, ?, ?, ?, ?)', params);
 
       con.query(insertUserSQL, function (err, result) {
         if (err) { 
@@ -113,9 +157,20 @@ exports.creatuser = (req, res) => {
           return res.send(error);
         }
 
+        //translate from req.body to display object
+        let display = {};
+        display.employeeId = result.insertId;
+        display.firstName = user.firstName;
+        display.lastName = user.lastName;
+        display.email = user.email;
+        display.dateOfBirth = dateOfBirth;
+        display.employeeNumber = user.employeeNumber;
+        display.seniority = null; //TODO: figure out how to set seniority
+        display.username = user.username;
+
         req.session.regenerate(() => {
-          req.session.username = req.body.username; //store _id in session
-          return res.send({message: `Welcome: ${req.body.username}`});
+          req.session.username = req.body.username; //store username in session
+          return res.send(display);
         });
       });
     });
