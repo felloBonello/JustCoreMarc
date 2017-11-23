b1775b7e50e2090a240cbb1f674c0536d14b3f44
Marcus Baldassarre <baldassarremarcus@gmail.com>
2017-11-23 03:40:30 -0500

    Added back button

--- Client/src/app/work/runs-detail.component.ts
+++ Client/src/app/work/runs-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { WorkItem } from './work-item';
 
 @Component({
@@ -10,5 +10,6 @@ export class RunsDetailComponent {
     this.selectedWorkItem = (<any>Object).assign({}, value);
   }
   @Input() workItems: WorkItem[];
+  @Output() cancelled = new EventEmitter();
   selectedWorkItem: WorkItem;
 }
